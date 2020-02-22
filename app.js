69ff1133e0330ad80fce2ca076f639c456321680
Biplab Thapa Magar <biplab.t.magar@gmail.com>
2020-02-22 16:42:38 -0500

    Added inventory page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var inventoryRouter = require('./routes/inventory')
 
 var app = express();
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/inventory', inventoryRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
--- routes/inventory.js
+++ routes/inventory.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET inventory page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.sendfile('./public/inventory.html');
 });
 
 module.exports = router;
