f64a8f037c256c92dce6fac73902c72221d83544
Biplab Thapa Magar <biplab.t.magar@gmail.com>
2020-02-22 18:07:31 -0500

    new commit

---
+++ public/javascripts/inventoryScript.js
@@ -0,0 +1,4 @@
+aisleList = ["Aisle 1 - Organics", "Aisle 2 - Cleaning Products", "Aisle 3 - Pet Food",
+            "Aisle 4 - Bread", "Aisle 5 - Meat", "Aisle 6 - Frozen Food", "Aisle 7 - Miscellaneous"];
+
+let 
