fcf47dce450ac1f6efb1cca701c6d1f12670c666
Biplab Thapa Magar <biplab.t.magar@gmail.com>
2020-02-22 18:56:34 -0500

    added client side javascript for inventory list page

--- public/javascripts/inventoryScript.js
+++ public/javascripts/inventoryScript.js
@@ -1,4 +1,16 @@
-aisleList = ["Aisle 1 - Organics", "Aisle 2 - Cleaning Products", "Aisle 3 - Pet Food",
-            "Aisle 4 - Bread", "Aisle 5 - Meat", "Aisle 6 - Frozen Food", "Aisle 7 - Miscellaneous"];
+let aisleArray = ["Aisle 1 - Organics", "Aisle 2 - Cleaning Products", "Aisle 3 - Pet Food",
+            "Aisle 4 - Bread", "Aisle 5 - Meat", "Aisle 6 - Frozen Food", "Aisle 7 - Miscellaneous",
+            "Aisle 8 - Soda/Energy Drink"];
 
-let 
+let aisleListContainer = document.querySelector("ul.list-group");
+
+aisleArray.forEach(function(item, index){
+    var aisleELement = document.createElement('li');
+    aisleELement.classList.add("list-group-item");
+    aisleELement.classList.add("aisles");
+    var aisleName = document.createElement('h3');
+    aisleName.innerHTML = "▶ " + item;
+    aisleELement.appendChild(aisleName);
+    aisleListContainer.appendChild(aisleELement);
+});
+
