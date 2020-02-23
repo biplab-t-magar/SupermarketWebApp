df57c31d1858f8e456a9601183ad3b6c18535197
Srijan Joshi <srijan1214@gmail.com>
2020-02-23 01:28:47 -0500

    Created Employee html

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var inventoryRouter = require('./routes/inventory')
 
 var app = express();
@@ -17,6 +16,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/inventory', inventoryRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
